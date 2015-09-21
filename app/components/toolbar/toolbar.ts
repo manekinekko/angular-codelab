4fcf594e301bcaafe9e12142d330888b2fcdcf0c
Wassim Chegham <maneki.nekko@gmail.com>
2015-09-21 23:39:59 +0200

    demonstrate the use of directives: add "fix-scrolling" directive

--- app/components/toolbar/toolbar.ts
+++ app/components/toolbar/toolbar.ts
@@ -1,17 +1,52 @@
 /// <reference path="../../typings/_custom.d.ts" />
 
-import {Component, View, bootstrap, ViewEncapsulation} from 'angular2/angular2';
+import {
+  Component, 
+  View, 
+  bootstrap, 
+  ViewEncapsulation, 
+  Directive,
+  Host,
+  SkipSelf,
+  ElementRef,
+} from 'angular2/angular2';
+
+import {DOM} from 'angular2/src/core/dom/dom_adapter';
 
 import {Home} from '../home/home';
+
+
+@Directive({
+  selector: '[fix-scrolling]',
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
+})
+class FixScrolling {
+  
+  private body: HTMLBodyElement;
+  
+  constructor(private elementRef: ElementRef) {
+    this.body = DOM.query('body');
+    this.setHeight();
+  }
+  
+  onResize(event: Event) {
+    this.setHeight();
+  }
+  
+  private setHeight() {
+    let height = this.body.getBoundingClientRect().height;
+    DOM.setStyle(this.elementRef.nativeElement, 'height', `${ height }px`);
+  }
+}
 
 @Component({
   selector: 'toolbar'
 })
 @View({
   templateUrl: 'components/toolbar/toolbar.html',
-  directives: [ Home ],
-  encapsulation: ViewEncapsulation.None,
-  styleUrls: ['vendor/material.css']
+  directives: [ Home, FixScrolling ]
 })
 export class Toolbar {
   title: string = 'Toolbar';
