dd70367444435a31dfaee59c2dec329b1847527f
Wassim Chegham <maneki.nekko@gmail.com>
2015-10-29 00:44:18 +0100

    remove unused imports

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,6 +1,6 @@
 /// <reference path="typings/_custom.d.ts" />
 
-import { bind, provide, bootstrap } from 'angular2/angular2';
+import { provide, bootstrap } from 'angular2/angular2';
 import { ROUTER_PROVIDERS, HashLocationStrategy, LocationStrategy } from 'angular2/router';
 import { Devfest } from './app';
 
