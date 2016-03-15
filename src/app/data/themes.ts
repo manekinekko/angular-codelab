bb8a3b6410278f0a816740b09b0dcdd95105d967
cbalit <dou4872>
2016-03-15 11:35:15 +0100

    add themes datas and service

---
+++ src/app/data/themes.ts
@@ -0,0 +1,27 @@
+import { Chance } from 'chance';
+
+export const THEMES = (() => {
+
+  return [{
+    "slug": "typescript",
+    "title": "TypeScript",
+    "logo": "images/typescript.png",
+    "description": chance.word()
+  }, {
+    "slug": "ecma",
+    "title": "ECMAScript 6",
+    "logo": "images/js.jpg",
+    "description": chance.word()
+  }, {
+    "slug": "ng2",
+    "title": "Angular 2",
+    "logo": "images/angular2.jpg",
+    "description": chance.word()
+  }, {
+    "slug": "react",
+    "title": "React",
+    "logo": "images/react.svg",
+    "description": chance.word()
+  }];
+
+})();
