9e8da8b11c11cdf1a27ce957496a72b22ab9b88f
Wassim Chegham <github@wassimchegham.com>
2016-03-19 15:59:30 +0100

    refacto(ng2-codelab): clean code

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,7 +1,7 @@
 import { Component, ViewEncapsulation } from 'angular2/core';
 
 import { ThemeCard } from '../theme-card/theme-card';
-import { TechnologiesStore, ITechnology } from '../../services/technologies-store/technologies-store';
+import { TechnologiesStore } from '../../services/technologies-store/technologies-store';
 
 @Component({
   selector: 'home',
@@ -15,13 +15,6 @@ export class Home {
   private themeCards: any[];
 
   constructor(technologiesStore: TechnologiesStore){
-    technologiesStore.fetch().then((themes) => {
-      this.themeCards = themes;
-    });
+    technologiesStore.fetch().then((themes) => this.themeCards = themes);
   }
-
-  updateTitle() {
-    //console.log(arguments);
-  }
-
 }
