68a9b948f9424b886ede6a273c83b4b872437bb2
Wassim Chegham <github@wassimchegham.com>
2016-04-18 14:59:06 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import { bootstrap } from 'angular2/platform/browser'
 import { Ng2CodelabApp } from './app/ng2-codelab';
 
-bootstrap(Ng2CodelabApp,[]);
+bootstrap(Ng2CodelabApp,[
+  ROUTER_PROVIDERS,
+	provide(LocationStrategy, {useClass: PathLocationStrategy})
+]);
