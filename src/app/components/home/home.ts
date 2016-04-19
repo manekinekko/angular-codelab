fc0482c133d3080610a62268692bda907df32717
Wassim Chegham <github@wassimchegham.com>
2016-04-19 11:57:33 +0200

    Create home.ts

---
+++ src/app/components/home/home.ts
@@ -0,0 +1,20 @@
+import { Component, ViewEncapsulation } from 'angular2/core';
+
+import { ThemeCard } from '../theme-card/theme-card';
+import { TechnologiesStore } from '../../services/technologies-store/technologies-store';
+
+@Component({
+  selector: 'home',
+  providers: [ TechnologiesStore ],
+  templateUrl: './app/components/home/home.html',
+  directives: []
+})
+export class Home {
+
+  private themeCards: any[];
+
+  constructor(technologiesStore: TechnologiesStore){
+    technologiesStore.fetch().then((themes) => this.themeCards = themes);
+  }
+
+}
